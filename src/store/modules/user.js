bed8ed5979d34d7eee1ecd62b22f5b416915fa08
“hyb” <3164249018@qq.com>
2022-02-16 22:42:21 +0800

    注册登录修改

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -29,18 +29,16 @@ const mutations = {
 
 const actions = {
   // user login
-  login({ commit }, userInfo) {
+  async login({ commit }, userInfo) {
     const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+    let res = await login({ username: username.trim(), password: password })
+    // console.log(res)
+    if(res.code === 20000 || res.code === 200){
+      commit('SET_TOKEN', res.data.token)
+      setToken(res.data.token)
+    } else {
+      return Promise.reject(new Error("fail"))
+    }
   },
 
   // get user info
