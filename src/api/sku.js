ccad12d0e674ac8cb2b8e67cb8dbfe31eb1c2a48
HeyinB <3164249018@qq.com>
2022-02-25 11:27:25 +0800

    sku详情

--- src/api/sku.js
+++ src/api/sku.js
@@ -4,50 +4,65 @@ import request from '@/utils/request'
 // /admin/product/spuImageList/{spuId}
 
 
-export const reqSpuImageList=(spuId)=>request({
-    url:`/admin/product/spuImageList/${spuId}`,
+export const reqSpuImageList = (spuId) => request({
+    url: `/admin/product/spuImageList/${spuId}`,
     method: "get"
 })
 
 
 // /admin/product/spuSaleAttrList/{spuId}
 
-export const reqSpuSaleAttrList=(spuId)=>request({
-    url:`/admin/product/spuSaleAttrList/${spuId}`,
+export const reqSpuSaleAttrList = (spuId) => request({
+    url: `/admin/product/spuSaleAttrList/${spuId}`,
     method: "get"
 })
 
 // /admin/product/attrInfoList/{category1Id}/{category2Id}/{category3Id}
 
-export const reqAttrInfoList=(category1Id,category2Id,category3Id)=>request({
-    url:`/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
+export const reqAttrInfoList = (category1Id, category2Id, category3Id) => request({
+    url: `/admin/product/attrInfoList/${category1Id}/${category2Id}/${category3Id}`,
     method: "get"
 })
 
-export const reqAddSku=(skuInfo)=>request({
-    url:`/admin/product/saveSkuInfo`,
+export const reqAddSku = (skuInfo) => request({
+    url: `/admin/product/saveSkuInfo`,
     method: "post",
-    data:skuInfo
+    data: skuInfo
 })
 
-export const reqSpuList=(spuId)=>request({
-    url:`/admin/product/findBySpuId/${spuId}`,
+export const reqSpuList = (spuId) => request({
+    url: `/admin/product/findBySpuId/${spuId}`,
     method: "get"
 })
 
 //sku列表接口
 
 // /admin/product/list/{page}/{limit}
-export const reqSkuList=(page,limit) =>request({
-    url:`/admin/product/list/${page}/${limit}`,
-    method:'get'
+export const reqSkuList = (page, limit) => request({
+    url: `/admin/product/list/${page}/${limit}`,
+    method: 'get'
 })
 
 //上架
 // /admin/product/onSle/{skuId}
-
+export const reqSale = (skuId) => request({
+    url: `/admin/product/onSale/${skuId}`,
+    method: "get"
+})
 
 
 
 //下架
-// /admin/product/list/{page}/{limit}
+// /admin/product/cancelSale/{skuId}
+export const reqCancel = (skuId) => request({
+    url: `/admin/product/cancelSale/${skuId}`,
+    method: 'get'
+})
+
+
+// 抽屉  /admin/product/getSkuById/{skuId}
+
+export const reqGetSkuById = (skuId) => request({
+    url: `/admin/product/getSkuById/${skuId}`,
+    method: 'get'
+})
--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -24,16 +24,28 @@
             type="success"
             size="mini"
             icon="el-icon-sort-down"
+            @click="sale(row)"
             v-show="row.isSale == 0"
           ></el-button>
           <el-button
             type="success"
             size="mini"
             icon="el-icon-sort-up"
+            @click="cancel(row)"
             v-show="row.isSale != 0"
           ></el-button>
-          <el-button type="primary" size="mini" icon="el-icon-edit"></el-button>
-          <el-button type="info" size="mini" icon="el-icon-info"></el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-edit"
+            @click="edit"
+          ></el-button>
+          <el-button
+            type="info"
+            size="mini"
+            icon="el-icon-info"
+            @click="reqGetSkuById(row)"
+          ></el-button>
           <el-button
             type="danger"
             size="mini"
@@ -55,6 +67,42 @@
       :total="total"
     >
     </el-pagination>
+
+    <el-drawer :visible.sync="drawer" :show-close="false" size="50%">
+      <el-row>
+        <el-col :span="5">名称</el-col>
+        <el-col :span="16">{{ skuInfo.skuName }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">描述</el-col>
+        <el-col :span="16">{{ skuInfo.skuDesc }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">价格</el-col>
+        <el-col :span="16">{{ skuInfo.price }}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">平台属性</el-col>
+        <el-col :span="16">
+          <el-tag
+            style="margin-right: 10px"
+            v-for="attr in skuInfo.skuAttrValueList"
+            :key="attr.id"
+            >{{ attr.attrId }}-{{ attr.valueId }}</el-tag
+          >
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">商品图片</el-col>
+        <el-col :span="16">
+          <el-carousel height="150px">
+            <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+              <img :src="item.imgUrl" alt="" style="width:100%;height:150px;"/>
+            </el-carousel-item>
+          </el-carousel>
+        </el-col>
+      </el-row>
+    </el-drawer>
   </div>
 </template>
 <script>
@@ -66,6 +114,8 @@ export default {
       limit: 3,
       skuList: [],
       total: 0,
+      skuInfo: {},
+      drawer: false,
     };
   },
   mounted() {
@@ -85,9 +135,67 @@ export default {
       this.limit = limit;
       this.getskuList();
     },
+    async sale(row) {
+      let res = await this.$API.sku.reqSale(row.id);
+      if (res.code == 200) {
+        row.isSale = 1;
+        this.$message({ type: "success", message: "上架成功" });
+      }
+    },
+    async cancel(row) {
+      let res = await this.$API.sku.reqCancel(row.id);
+      if (res.code == 200) {
+        row.isSale = 0;
+        this.$message({ type: "success", message: "下架成功" });
+      }
+    },
+    edit() {
+      this.$message.info("正在开发中");
+    },
+    async reqGetSkuById(row) {
+      let res = await this.$API.sku.reqGetSkuById(row.id);
+      if (res.code == 200) {
+        this.skuInfo = res.data;
+        this.drawer = true;
+      }
+    },
   },
 };
 </script>
   
 <style scoped>
+.el-row .el-col-5 {
+  font-size: 18px;
+  text-align: right;
+}
+.el-col {
+  margin: 10px 10px;
+}
+
+</style>
+
+<style>
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.el-carousel__button{
+  width: 10px;
+  height: 10px;
+  background-color: red;
+  border-radius: 50%;
+}
 </style>
