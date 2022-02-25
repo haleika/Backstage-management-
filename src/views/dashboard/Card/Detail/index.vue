94f5ffc28ebc259088a26b247aa77dbf9cdb8e55
HeyinB <3164249018@qq.com>
2022-02-25 15:39:20 +0800

    首页头部静态

---
+++ src/views/dashboard/Card/Detail/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-card>
+    <div class="card-header">
+      <span>{{title}}</span>
+      <svg t="1645771726012" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1223" width="16" height="16"><path d="M512 958.016C266.08 958.016 65.984 757.952 65.984 512 65.984 266.08 266.08 65.984 512 65.984c245.952 0 446.016 200.064 446.016 446.016C958.016 757.952 757.952 958.016 512 958.016zM512 129.984C301.344 129.984 129.984 301.344 129.984 512c0 210.624 171.36 382.016 382.016 382.016 210.624 0 382.016-171.36 382.016-382.016C894.016 301.344 722.624 129.984 512 129.984z" p-id="1224"></path><path d="M512 304m-48 0a1.5 1.5 0 1 0 96 0 1.5 1.5 0 1 0-96 0Z" p-id="1225"></path><path d="M512 768c-17.664 0-32-14.304-32-32l0-288c0-17.664 14.336-32 32-32s32 14.336 32 32l0 288C544 753.696 529.664 768 512 768z" p-id="1226"></path></svg>
+    </div>
+    <div class="card-content">{{count}}</div>
+    <div class="card-charts">
+        <slot name="charts"></slot>
+    </div>
+    <div class="card-footer">
+        <slot name="footer"></slot>
+    </div>
+  </el-card>
+</template>
+    
+<script>
+export default {
+  name: "Detail",
+  props: ["title","count"],
+};
+</script>
+
+<style scoped>
+.card-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #ccc;
+    font-size: 16px;
+}
+.card-content{
+    font-size: 30px;
+    margin: 10px 0;
+}
+.card-charts{
+    height: 40px;
+}
+.card-footer{
+    border-top: 1px solid #eee;
+}
+</style>
+    
---
+++ src/views/dashboard/Card/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <Detail title="总销售额" count="88460">
+          <template slot="charts">
+            <span
+              >周同比&nbsp;&nbsp;12% <span class="el-icon-sort-up"></span>
+            </span>
+            <span
+              >日同比&nbsp;&nbsp;12% <span class="el-icon-sort-down"></span>
+            </span>
+          </template>
+          <template slot="footer">
+            <span>日销售额￥123658</span>
+          </template>
+        </Detail>
+      </el-col>
+      <el-col :span="6">
+        <Detail title="访问量" count="88460">
+          <template slot="charts"> </template>
+          <template slot="footer">
+            <span>日访问量123658</span>
+          </template>
+        </Detail></el-col
+      >
+      <el-col :span="6">
+        <Detail title="支付笔数" count="88460">
+          <template slot="charts"> </template>
+          <template slot="footer">
+            <span>转化率123658</span>
+          </template>
+        </Detail>
+      </el-col>
+      <el-col :span="6">
+        <Detail title="运营活动效果" count="88460">
+          <template slot="charts"> </template>
+          <template slot="footer">
+            <span
+              >周同比&nbsp;&nbsp;12% <span class="el-icon-sort-up"></span>
+            </span>
+            <span
+              >日同比&nbsp;&nbsp;12% <span class="el-icon-sort-down"></span>
+            </span>
+          </template>
+        </Detail>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+    
+<script>
+import Detail from "./Detail";
+export default {
+  name: "Card",
+  components: { Detail },
+};
+</script>
+    
